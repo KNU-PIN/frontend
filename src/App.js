3c88e091dfdabe2d560fb95111061b2b4df56a38
고동재 <ehdwo98@knu.ac.kr>
2023-02-09 21:01:47 +0900

    feat: 글쓰기 버튼 클릭시 글쓰기 페이지로 라우트

--- src/App.js
+++ src/App.js
@@ -1,3 +1,38 @@
-function App() {}
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+
+function App() {
+  const navigate = useNavigate();
+  return (
+    <div className="App">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <button
+                onClick={() => {
+                  navigate("/page");
+                }}
+              >
+                글쓰기
+              </button>
+            </>
+          }
+        />
+        <Route path="/page" element={<Event />} />
+        <Route path="*" element={<div>404page</div>} />
+      </Routes>
+    </div>
+  );
+}
+
+function Event() {
+  return (
+    <div>
+      <h2>글쓰기 페이지</h2>
+      <Outlet />
+    </div>
+  );
+}
 
 export default App;
