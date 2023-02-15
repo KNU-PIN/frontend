bd2079557db9a2744fce469b17a4563bba23217f
seokiis <cat1181123@naver.com>
2023-02-16 01:26:51 +0900

    feature: 지도에 핀 띄우기 구현

--- src/components/atoms/showpins.jsx
+++ src/components/atoms/showpins.jsx
@@ -1,6 +1,16 @@
+import pinData from "../../pinData.json";
+
 const { kakao } = window;
 
-export const getMap = () => {
+//pin데이터에 카카오 위도 경도 추가
+function addKakaoLatLng() {
+    pinData.map(function (pin) {
+        pin.latlng = new kakao.maps.LatLng(pin.latitude, pin.longitude);
+        return pin;
+    });
+}
+
+export const showPins = () => {
     const container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
     const options = {
         //지도를 생성할 때 필요한 기본 옵션
@@ -10,31 +20,16 @@ export const getMap = () => {
 
     const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
 
-    //핀이 찍힐 위치 입니다.
-    var positions = [
-        {
-            title: "카카오",
-            latlng: new kakao.maps.LatLng(33.450705, 126.570677),
-        },
-        {
-            title: "생태연못",
-            latlng: new kakao.maps.LatLng(33.450936, 126.569477),
-        },
-        {
-            title: "텃밭",
-            latlng: new kakao.maps.LatLng(33.450879, 126.56994),
-        },
-        {
-            title: "근린공원",
-            latlng: new kakao.maps.LatLng(33.451393, 126.570738),
-        },
-    ];
+    //카카오 위도 경도 추가
+    addKakaoLatLng();
+    console.log(pinData);
 
     // 핀 이미지 주소입니다.
     var imageSrc =
         "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
 
-    for (var i = 0; i < positions.length; i++) {
+    //지도에 핀 표시
+    for (var i = 0; i < pinData.length; i++) {
         // 핀 이미지의 이미지 크기 입니다
         var imageSize = new kakao.maps.Size(24, 35);
 
@@ -44,8 +39,8 @@ export const getMap = () => {
         // 마커를 생성합니다
         var marker = new kakao.maps.Marker({
             map: map, // 마커를 표시할 지도
-            position: positions[i].latlng, // 마커를 표시할 위치
-            title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+            position: pinData[i].latlng, // 마커를 표시할 위치
+            //title: pinData[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
             image: markerImage, // 마커 이미지
         });
 
--- src/pages/Pinboard.jsx
+++ src/pages/Pinboard.jsx
@@ -6,17 +6,20 @@ import { useState } from "react";
 import React from "react";
 import { useEffect } from "react";
 import pins from "../pinData";
-import { getMap } from "../components/atoms/getMap";
+import { showPins } from "../components/atoms/showpins";
 import Map from "../components/atoms/Map";
 
 const { kakao } = window;
 
 export default function Pinboard() {
-    //지도
+    //지도 및 핀 표시
     useEffect(() => {
-        getMap();
+        showPins();
     }, []);
 
+    //목데이터를 받아서
+
+    //가공해서 ()
     const navigate = useNavigate();
 
     const [search, setSearch] = useState("");
@@ -28,6 +31,7 @@ export default function Pinboard() {
         <>
             <Layout title="Main Page">
                 <FormWrapper>
+                    {/* 지도 틀 */}
                     <Map></Map>
                 </FormWrapper>
             </Layout>
