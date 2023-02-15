bb7fe565ab02b8d3381dbb34ff73191185773da5
seokiis <cat1181123@naver.com>
2023-02-16 03:56:19 +0900

    feat: pin color 추가

--- src/components/atoms/Map.jsx
+++ src/components/atoms/Map.jsx
@@ -1,5 +1,16 @@
+import styled from "styled-components";
+
+const FreeButton = styled.button`
+    position: absolute;
+    z-index: 100;
+`;
+
 function Map() {
-    return <div id="map" style={{ width: "500px", height: "400px" }}></div>;
+    return (
+        <div id="map" style={{ width: "500px", height: "600px" }}>
+            <FreeButton> 내 위치 찾기</FreeButton>
+        </div>
+    );
 }
 
 export default Map;
--- src/components/atoms/showpins.jsx
+++ src/components/atoms/showpins.jsx
@@ -1,6 +1,9 @@
 import pinData from "../../pinData.json";
 
+import { useState } from "react";
+
 const { kakao } = window;
+// const { pin, setPin } = useState("");
 
 //pin데이터에 카카오 위도 경도 추가
 function addKakaoLatLng() {
@@ -20,29 +23,59 @@ export const showPins = () => {
 
     const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
 
+    var redImage = process.env.PUBLIC_URL + "img/red.png";
+    var greenImage = process.env.PUBLIC_URL + "img/green.png";
+    var yellowImage = process.env.PUBLIC_URL + "img/yellow.png";
+
     //카카오 위도 경도 추가
     addKakaoLatLng();
-    console.log(pinData);
-
-    // 핀 이미지 주소입니다.
-    var imageSrc =
-        "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
 
     //지도에 핀 표시
     for (var i = 0; i < pinData.length; i++) {
         // 핀 이미지의 이미지 크기 입니다
-        var imageSize = new kakao.maps.Size(24, 35);
-
-        // 핀 이미지를 생성합니다
-        var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+        var imageSize = new kakao.maps.Size(30, 30);
 
         // 마커를 생성합니다
-        var marker = new kakao.maps.Marker({
-            map: map, // 마커를 표시할 지도
-            position: pinData[i].latlng, // 마커를 표시할 위치
-            //title: pinData[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-            image: markerImage, // 마커 이미지
-        });
+        switch (pinData[i].type) {
+            case "red":
+                // 핀 이미지를 생성합니다
+                var markerImage = new kakao.maps.MarkerImage(
+                    redImage,
+                    imageSize
+                );
+                //핀 표시
+                var marker = new kakao.maps.Marker({
+                    map: map, // 마커를 표시할 지도
+                    position: pinData[i].latlng, // 마커를 표시할 위치
+                    //title: pinData[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+                    image: markerImage, // 마커 이미지
+                });
+                break;
+            case "green":
+                //핀 이미지
+                markerImage = new kakao.maps.MarkerImage(greenImage, imageSize);
+                //핀 표시
+                marker = new kakao.maps.Marker({
+                    map: map, // 마커를 표시할 지도
+                    position: pinData[i].latlng, // 마커를 표시할 위치
+                    //title: pinData[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+                    image: markerImage, // 마커 이미지
+                });
+                break;
+            default:
+                //핀 이미지
+                markerImage = new kakao.maps.MarkerImage(
+                    yellowImage,
+                    imageSize
+                );
+                //핀 표시
+                marker = new kakao.maps.Marker({
+                    map: map, // 마커를 표시할 지도
+                    position: pinData[i].latlng, // 마커를 표시할 위치
+                    //title: pinData[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+                    image: markerImage, // 마커 이미지
+                });
+        }
 
         marker.setMap(map);
     }
