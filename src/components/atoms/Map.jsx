73750b29a64515d0f619ac2e6818bf2d6ac5c53c
seokiis <cat1181123@naver.com>
2023-02-16 16:43:53 +0900

    button 추가

--- src/components/atoms/Map.jsx
+++ src/components/atoms/Map.jsx
@@ -1,13 +1,25 @@
 import styled from "styled-components";
 
 const MapWrap = styled.button`
+    //카테고리 버튼들의 absolute 정렬을 위해 realtive 필요함.
     position: relative;
+
+    //안에 있는 아이템 가운데 정렬?
+    display: flex;
+    justify-content: center;
+
     overflow: hidden;
     width: 100%;
     height: 100%;
 `;
 
-const Div = styled.div`
+const Category = styled.div`
+    display: flex;
+    z-index: 1;
+`;
+
+const Free = styled.button`
+    color: white;
     position: absolute;
     //글씨 중간 정렬
     display: flex;
@@ -27,9 +39,48 @@ const Div = styled.div`
     background-color: #f06363;
 `;
 
-const HI = styled.span`
-    color: #fff;
-`;
+// Wanted 랑 Market은 아직 건드리지 않았어
+
+// const Wanted = styled.button`
+//     color: white;
+//     position: absolute;
+//     //글씨 중간 정렬
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+//     top: 50px;
+//     left: 10px;
+//     /* overflow: hidden; */
+//     width: 130px;
+//     height: 30px;
+//     /* margin: 0;
+//     padding: 0; */
+//     z-index: 1;
+//     font-size: 12px;
+//     border: none;
+//     border-radius: 5px;
+//     background-color: #f06363;
+// `;
+// const Market = styled.button`
+//     color: white;
+//     position: absolute;
+//     //글씨 중간 정렬
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+//     top: 50px;
+//     left: 10px;
+//     /* overflow: hidden; */
+//     width: 130px;
+//     height: 30px;
+//     /* margin: 0;
+//     padding: 0; */
+//     z-index: 1;
+//     font-size: 12px;
+//     border: none;
+//     border-radius: 5px;
+//     background-color: #f06363;
+// `;
 
 function Map() {
     return (
@@ -43,9 +94,12 @@ function Map() {
                     overflow: "hidden",
                 }}
             ></div>
-            <Div>
-                <HI onclick="">지도</HI>
-            </Div>
+            <Category>
+                <Free onclick="">자유글</Free>
+                {/* <Wanted onclick="">구인구직글</Wanted>
+                <Market onclick="">지도</Market> */}
+                ㄴ
+            </Category>
         </MapWrap>
     );
 }
