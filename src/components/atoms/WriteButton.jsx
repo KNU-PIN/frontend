c8d4082facb14e059a4fc7cc4470fc3f107eeaa5
seokiis <cat1181123@naver.com>
2023-02-21 21:40:36 +0900

    fix: delete yarn.lock

--- src/components/atoms/WriteButton.jsx
+++ src/components/atoms/WriteButton.jsx
@@ -3,36 +3,40 @@ import { useNavigate } from "react-router-dom";
 import WriteModal from "./WriteModal";
 import React, { useState, useRef } from "react";
 export default function WriteButton() {
-  const navigate = useNavigate();
-  const [modalOpen, setModalOpen] = useState(false);
+    const navigate = useNavigate();
+    const [modalOpen, setModalOpen] = useState(false);
 
-  function showModal() {
-    setModalOpen(true);
-  }
-  return (
-    <>
-      <StyledButton
-        onClick={() => {
-          showModal();
-        }}
-      >
-        게시글 작성
-      </StyledButton>
-      <StyledDiv>{modalOpen && <WriteModal setModalOpen={setModalOpen}></WriteModal>}</StyledDiv>
-    </>
-  );
+    function showModal() {
+        setModalOpen(true);
+    }
+    return (
+        <>
+            <StyledButton
+                onClick={() => {
+                    showModal();
+                }}
+            >
+                게시글 작성
+            </StyledButton>
+            <StyledDiv>
+                {modalOpen && (
+                    <WriteModal setModalOpen={setModalOpen}></WriteModal>
+                )}
+            </StyledDiv>
+        </>
+    );
 }
 const StyledDiv = styled.div`
-  display: flex;
-  justify-content: center;
+    display: flex;
+    justify-content: center;
 `;
 const StyledButton = styled.button`
-  border:1px solid black;
-  font-size: 1rem;
-  font-weight: bold;
-  background-color: transparent;
+    border: 1px solid black;
+    font-size: 1rem;
+    font-weight: bold;
+    background-color: transparent;
 
-  left:45%
-  bottom:5px;
-  position:fixed;
+    left: 45%;
+    bottom: 5px;
+    position: fixed;
 `;
